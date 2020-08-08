5c19e65c269cfb2a483586bc0b9afb3256af74de
Dor <18174994+ScDor@users.noreply.github.com>
2020-08-08 14:57:29 +0300

    creates new spreadsheets for each call

--- main.py
+++ main.py
@@ -1,47 +1,125 @@
+from dataclasses import dataclass
+from datetime import datetime
 import gspread
-SPREADSHEET_KEY = "1CFCmrvcCiBng64ltEQ0usIxGyDIcLMwG3Tak67ugOrk"
+import logging
+from config import *
+from hebrew_phrases import *
 
-SUBJECT = "נושא"
-SUBJECT2 = f"{SUBJECT} 2"
-SUBJECT3 = f"{SUBJECT} 3"
-SUBJECTS = [SUBJECT, SUBJECT2, SUBJECT3]
-TYPE = "סוג"
-QUESTION_BODY = "שאלה"
+UNKNOWN_USER_NAME = "?"
+
+MISSING_TOPIC = "FOO"
+
+MAX_QUESTIONS = 5
+UNKNOWN_USER = "?"
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s\t%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 gc = gspread.service_account("credentials.json")
-file = gc.open_by_key(SPREADSHEET_KEY)
+logging.info("google login successful")
 
-types = file.worksheet("types").col_values(1)
-topics = file.worksheet("topics").col_values(1)
-queries = file.worksheet("query").get_all_records()
+logging.debug("reading files")
+question_file = gc.open_by_key(QUESTION_SPREADSHEET_KEY)
+ui_file = gc.open_by_key(UI_SPREADSHEET_KEY)
 
-questions = file.worksheet("questions").get_all_records()
-for q in questions:
-    q["topics"] = {topic for topic in [q[SUBJECT], q[SUBJECT2], q[SUBJECT3]] if topic}
-    if not q["topics"]:
-        print(f"ERROR: NO TOPIC for {q[QUESTION_BODY]}")
-        exit(1)
-    for key in SUBJECTS:
-        del q[key]
+logging.debug("reading question types and topics")
+types = question_file.worksheet("types").col_values(1)
+topics = question_file.worksheet("topics").col_values(1)
 
-# todo clear old results
-query_sheet = file.worksheet("query")
+question_sheet = question_file.worksheet("questions")
+query_sheet = ui_file.worksheet("query")
 
-found_questions = set()
-for i in range(len(queries)):
-    query = queries[i]
-    if not query[SUBJECT]:
-        continue
-    query_result = []
-    for question in questions:
-        if question[QUESTION_BODY] in found_questions:
-            continue
 
-        if query[SUBJECT] in question["topics"] and \
-                (not query[TYPE]  # query doesn't specify type
-                 or query[TYPE] == question[TYPE]):
-            query_result.append(question[QUESTION_BODY])
-            found_questions.add(question[QUESTION_BODY])
+@dataclass
+class QuestionHandler:
+    def __init__(self, sheet):
+        questions = sheet.get_all_records()
+        for q in questions:
+            q["topics"] = {topic for topic in [q[SUBJECT], q[SUBJECT2], q[SUBJECT3]]
+                           if topic}
+            if not q["topics"]:
+                logging.warning(f"no topic for question {q[QUESTION_BODY][:80]}")
+                q["topics"] = MISSING_TOPIC
 
-    for j in range(len(query_result)):
-        query_sheet.update_cell(2 + i, 3 + j, query_result[j])
+            for key in SUBJECTS:
+                del q[key]
+        self.questions = questions
+        logging.info(f"read {len(self.questions)} questions")
+
+
+question_handler = QuestionHandler(question_sheet)
+
+
+def create_query_dictionary(sheet):
+    keys = sheet.row_values(2)[:2]
+    queries = [q[:2] for q in sheet.get_all_values()[2:]]  # third row onwards
+    return [dict(zip(keys, v)) for v in queries]
+
+
+@dataclass
+class QueryHandler:
+
+    def __init__(self, ui_sheet):
+        output_name_column = 4
+
+        if ui_sheet.cell(1, 4).value == BUILDING_TEST:
+            ui_sheet.update_cell(1, output_name_column, UNKNOWN_USER_NAME)
+        self.current_user = ui_sheet.cell(1, output_name_column).value
+        ui_sheet.update_cell(1, output_name_column, BUILDING_TEST)
+
+        queries = create_query_dictionary(ui_sheet)
+
+        self.run_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+        self.output_file = gc.create(f"{self.run_time}_{self.current_user}", OUTPUT_FOLDER_ID)
+        self.output_sheet = self.output_file.sheet1
+        print(self.output_file.url)  # todo remove
+        self.log_run()
+
+        self.found_questions = set()
+        self.process_queries(queries)
+
+    def process_queries(self, queries):
+        for i in range(len(queries)):  # todo prettify
+            q = queries[i]
+            if not q[SUBJECT]:
+                continue
+            matching_questions = self.find_matching_questions(q)
+
+            self.output_sheet.update_cell(1, 1 + i, q[SUBJECT])
+            self.output_sheet.update_cell(2, 1 + i, q[TYPE])
+
+            if not matching_questions:
+                self.output_sheet.update_cell(3, 1 + i, NO_QUESTIONS_FOUND)
+                continue
+
+            for j in range(min(len(matching_questions), MAX_QUESTIONS)):
+                self.output_sheet.update_cell(3 + j, 1 + i, matching_questions[j])
+
+    def find_matching_questions(self, q):
+        result = []
+        for question in question_handler.questions:
+            body = question[QUESTION_BODY]
+            if body in self.found_questions:
+                continue
+
+            if q[SUBJECT] in question["topics"] and (not q[TYPE]  # q doesn't specify type
+                                                     or q[TYPE] == question[TYPE]):
+                result.append(body)
+                self.found_questions.add(body)
+        return result
+
+    def log_run(self):
+        output_url_column = 3
+        output_name_column = 4
+        output_time_column = 5
+
+        next_vacant_row = len(query_sheet.col_values(output_name_column)) + 1
+
+        query_sheet.update_cell(next_vacant_row, output_name_column, self.current_user)
+        query_sheet.update_cell(next_vacant_row, output_time_column, self.run_time)
+        query_sheet.update_cell(next_vacant_row, output_url_column, self.output_file.url)
+
+        query_sheet.update_cell(1, output_name_column, "")  # removes name
+
+
+
+query = QueryHandler(query_sheet)
